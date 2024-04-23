f17c21b1a3077cc427d9a6350580b66b4669b7ae
Rifky Aryo <110043806+rawp-id@users.noreply.github.com>
2024-04-23 07:10:41 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('register', [AuthController::class, 'showRegistrationForm']);
 Route::post('register', [AuthController::class, 'register']);
 Route::get('logout', [AuthController::class, 'logout']);
 
-Route::middleware([AdminMiddleware::class])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::Resource('vehicles', VehicleController::class);
     Route::Resource('drivers', DriverController::class);
     Route::Resource('bookings', BookingController::class);
@@ -30,11 +30,6 @@ Route::middleware([AdminMiddleware::class])->group(function () {
 });
 
 Route::middleware([SuperAdminMiddleware::class])->group(function () {
-    Route::Resource('vehicles', VehicleController::class);
-    Route::Resource('drivers', DriverController::class);
-    Route::Resource('bookings', BookingController::class);
-    Route::get('bookings/status/{booking:id}', [BookingController::class, 'status']);
-    Route::get('check', [ApprovalController::class, 'check']);
     Route::Resource('approvals', ApprovalController::class);
 });
 
